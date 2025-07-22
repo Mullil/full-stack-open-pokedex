cc101cd8f439b38e65f93e9e0c044bb1fbe07f88
Mullil <eetu.taatila@gmail.com>
2025-07-22 16:43:18 +0300

    Test webhook & break build

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+import NotReal from 'this-module-does-not-exist'
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
